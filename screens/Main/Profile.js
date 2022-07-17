17b859510cfe6413e751fbe637c9690b6a167ed0
KimHS0915 <remember2552@gmail.com>
2022-07-17 22:13:22 +0900

    Update Profile

--- screens/Main/Profile.js
+++ screens/Main/Profile.js
@@ -4,13 +4,66 @@ import styled from "styled-components/native";
 import api from "../../api";
 
 const Container = styled.View`
-  jstify-content: center;
-  align-items: center;
-  margin-top: 10px;
   padding: 10px;
+  flex: 1;
 `;
-const Text = styled.Text`
+
+const AvatarContainer = styled.View`
+  flex: 1;
+  margin-top: 20px;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  margin-bottom: 10px;
+  overflow: hidden;
+`;
+
+const AvatarPhoto = styled.Image`
+  border-radius: 90px;
+  width: 50%;
+  height: 100%;
+`;
+
+const InfoContainer = styled.View`
+  flex: 2;
+  margin-top: 10px;
+`;
+
+const EmailContainer = styled.View`
   padding: 5px;
+  align-items: center;
+`;
+
+const EmailText = styled.Text`
+  font-size: 20px;
+  font-weight: 600;
+s`;
+
+const NameContainer = styled.View`
+  margin-top: 20px;
+  align-items: center;
+`;
+
+const NameText = styled.Text`
+  font-size: 16px;
+  font-weight: 300;
+`;
+
+const SuperhostContainer = styled.View`
+  margin-top: 15px;
+  align-items: center;
+  padding: 4px;
+  border: 1px solid black;
+  border-radius: 4px;
+  margin-bottom: 5px;
+  margin-left: 130px;
+  margin-right: 129px;
+`;
+
+const SuperhostText = styled.Text`
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 10px;
 `;
 
 const Profile = ({ user }) => {
@@ -24,11 +77,28 @@ const Profile = ({ user }) => {
   }, []);
   return (
     <Container>
-      <Text>User Name : {userInfo.username}</Text>
-      <Text>Email : {userInfo.email}</Text>
-      <Text>First Name : {userInfo.first_name}</Text>
-      <Text>Last Name : {userInfo.last_name}</Text>
-      <Text>Superhost : {userInfo.superhost ? "Yes" : "No"}</Text>
+      <AvatarContainer>
+        {userInfo.avatar ? (
+          <AvatarPhoto source={{ uri: userInfo.avatar }} />
+        ) : (
+          <AvatarPhoto source={require("../../assets/defaultRoomPhoto.jpg")} />
+        )}
+      </AvatarContainer>
+      <InfoContainer>
+        <EmailContainer>
+          <EmailText>{userInfo.email}</EmailText>
+        </EmailContainer>
+        <NameContainer>
+          <NameText>
+            {userInfo.first_name} {userInfo.last_name}
+          </NameText>
+        </NameContainer>
+        {!userInfo.superhost ? (
+          <SuperhostContainer>
+            <SuperhostText>superhost</SuperhostText>
+          </SuperhostContainer>
+        ) : null}
+      </InfoContainer>
     </Container>
   );
 };
